19be66eafb5f2f7ff1d18669c84b58a318b799a5
Andre-SAEON <andre@saeon.ac.za>
2018-05-29 11:03:33 +0200

    Fixed redirect_url issue when not on localhost. Resolves #57

--- NCCRD.Clients.Web.React/app/js/components/Authentication/userManager.js
+++ NCCRD.Clients.Web.React/app/js/components/Authentication/userManager.js
@@ -1,12 +1,13 @@
 import { createUserManager } from 'redux-oidc';
 import { ssoBaseURL } from '../../constants/ssoBaseURL'
+import { siteBaseURL } from '../../constants/siteBaseURL'
 
 const userManagerConfig = {
     client_id: 'NCCRD_React_Client',
-    client_secret: '5FB072EFC225',
-    redirect_uri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/#/callback#`,
-    // post_logout_redirect_uri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/#/logout`,
-    //silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
+    //client_secret: '5FB072EFC225',
+    redirect_uri: siteBaseURL + '#/callback#',
+    post_logout_redirect_uri: siteBaseURL + '#/logout',
+    //silent_redirect_uri: siteBaseURL + '/silent_renew.html',
     //response_mode: 'form_post',
     response_type: 'id_token token',
     scope: 'openid profile email SAEON.NCCRD.Web.API',
