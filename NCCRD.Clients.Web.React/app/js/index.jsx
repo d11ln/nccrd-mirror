e88177de70cdc4ec21df062b13cdb5210c0e1033
Andre-SAEON <andre@saeon.ac.za>
2018-07-24 20:21:51 +0200

    Hide header with navbar=hidden

--- NCCRD.Clients.Web.React/app/js/index.jsx
+++ NCCRD.Clients.Web.React/app/js/index.jsx
@@ -45,8 +45,8 @@ import Header from './components/Base/Header.jsx'
 injectTapEventPlugin()
 
 const mapStateToProps = (state, props) => {
-    let { globalData: { loading } } = state
-    return { loading }
+  let { globalData: { loading } } = state
+  return { loading }
 }
 
 /**
@@ -54,84 +54,76 @@ const mapStateToProps = (state, props) => {
  */
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.getNavbar = this.getNavbar.bind(this)
-
-        this.state = { navbar: true }
-        if (location.toString().includes("navbar=hidden")) {
-            this.state = { navbar: false }
-            stripURLParam("navbar=hidden")
-        }
+    this.state = { navbar: true }
+    if (location.toString().includes("navbar=hidden")) {
+      this.state = { navbar: false }
+      stripURLParam("navbar=hidden")
     }
+  }
 
-    getNavbar() {
-        if (this.state.navbar) {
-            return <CustomNavbar />
-        }
-    }
+  render() {
 
-    render() {
+    let loaderWidth = 300
+    let loaderHeight = 165
 
-        let loaderWidth = 300
-        let loaderHeight = 165
+    let { navbar } = this.state
 
-        return (
-            <div className="container">
-                <Router>
-                    <div>
-                        <Header />
+    return (
+      <div className="container">
+        <Router>
+          <div>
 
-                        {this.getNavbar()}
+            {navbar && <Header />}
+            {navbar && <CustomNavbar />}
 
-                        {/* <section style={{ minHeight: "400px", backgroundImage: `url(${backdrop})`}}> */}
-                            <Switch>
-                                <Route path="/" component={Home} exact />
-                                <Route path="/projects" component={Projects} exact />
-                                <Route path="/projects/:id" component={ProjectDetails} exact />
-                                <Route path="/login" component={Login} exact />
-                                <Route path="/logout" component={Logout} exact />
-                                <Route path="/loggedout" component={LoggedOut} exact />
-                                <Route path="/callback" component={CallbackPage} />
-                            </Switch>
-                        {/* </section> */}
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/projects" component={Projects} exact />
+              <Route path="/projects/:id" component={ProjectDetails} exact />
+              <Route path="/login" component={Login} exact />
+              <Route path="/logout" component={Logout} exact />
+              <Route path="/loggedout" component={LoggedOut} exact />
+              <Route path="/callback" component={CallbackPage} />
+            </Switch>
 
-                        <div className="container-fluid">
-                            <div className="row">
-                                <div
-                                    hidden={!this.props.loading}
-                                    className="card"
-                                    style={{ height: (loaderHeight + "px"), width: (loaderWidth + 'px'), position: "fixed", left: ((window.innerWidth / 2) - (loaderWidth / 2)), top: ((window.innerHeight / 2) - (loaderHeight / 2)), zIndex: "99" }}>
+            <div className="container-fluid">
+              <div className="row">
+                <div
+                  hidden={!this.props.loading}
+                  className="card"
+                  style={{ height: (loaderHeight + "px"), width: (loaderWidth + 'px'), position: "fixed", left: ((window.innerWidth / 2) - (loaderWidth / 2)), top: ((window.innerHeight / 2) - (loaderHeight / 2)), zIndex: "99" }}>
 
-                                    <div className="card-body">
-                                        <label style={{ width: "100%", textAlign: "center", fontSize: "x-large", fontWeight: "bold", color: "#2BBBAD" }}>LOADING</label>
-                                        <br />
-                                        <span style={{ width: "100px", paddingLeft: ((loaderWidth / 2) - 50) }}>
-                                            <Spinner big multicolor />
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <ReactTooltip delayShow={700} />
-
-                    </div>
-
-                </Router>
+                  <div className="card-body">
+                    <label style={{ width: "100%", textAlign: "center", fontSize: "x-large", fontWeight: "bold", color: "#2BBBAD" }}>LOADING</label>
+                    <br />
+                    <span style={{ width: "100px", paddingLeft: ((loaderWidth / 2) - 50) }}>
+                      <Spinner big multicolor />
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
-        )
-    }
+
+            <ReactTooltip delayShow={700} />
+
+          </div>
+
+        </Router>
+      </div>
+    )
+  }
 }
 
 const ConnectedApp = connect(mapStateToProps)(App)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <OidcProvider store={store} userManager={userManager}>
-            <ConnectedApp />
-        </OidcProvider>
-    </Provider>,
-    document.getElementById('app')
+  <Provider store={store}>
+    <OidcProvider store={store} userManager={userManager}>
+      <ConnectedApp />
+    </OidcProvider>
+  </Provider>,
+  document.getElementById('app')
 )
