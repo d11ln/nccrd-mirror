851cb3f02ec734ee05e09629ec102c9d6c31135e
Andre-SAEON <andre@saeon.ac.za>
2018-10-24 14:49:29 +0200

    Fixed missing polygon filter SP

--- NCCRD_API/NCCRD.Services.DataV2/Migrations/20181008102804_Init.cs
+++ NCCRD_API/NCCRD.Services.DataV2/Migrations/20181008102804_Init.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
@@ -908,6 +909,23 @@ namespace NCCRD.Services.DataV2.Migrations
                 name: "IX_Sector_SectorTypeId",
                 table: "Sector",
                 column: "SectorTypeId");
+
+            //ADD POLYGON SP
+            StringBuilder storedProcedureCode = new StringBuilder();
+            storedProcedureCode.Append("CREATE PROCEDURE dbo.PolygonFilter" + Environment.NewLine);
+            storedProcedureCode.Append("@WKT VARCHAR(MAX)" + Environment.NewLine);
+            storedProcedureCode.Append("AS" + Environment.NewLine);
+            storedProcedureCode.Append("BEGIN" + Environment.NewLine);
+            storedProcedureCode.Append("DECLARE @QUERY VARCHAR(MAX) = " + Environment.NewLine);
+            storedProcedureCode.Append("'SELECT Project.*" + Environment.NewLine);
+            storedProcedureCode.Append("FROM  Location" + Environment.NewLine);
+            storedProcedureCode.Append("INNER JOIN ProjectLocation ON Location.LocationId = ProjectLocation.LocationId" + Environment.NewLine);
+            storedProcedureCode.Append("INNER JOIN Project ON ProjectLocation.ProjectId = Project.ProjectId" + Environment.NewLine);
+            storedProcedureCode.Append("WHERE geometry::STGeomFromText(''~WKT~'', 0).MakeValid().STIntersects(geometry::STGeomFromText(''POINT('' + CAST(Location.LonCalculated as VARCHAR(MAX)) + '' '' + CAST(Location.LatCalculated as VARCHAR(MAX)) + '')'', 0)) = 1';" + Environment.NewLine);
+            storedProcedureCode.Append("SET @QUERY = REPLACE(@QUERY, '~WKT~', @WKT)" + Environment.NewLine);
+            storedProcedureCode.Append("EXEC(@QUERY);" + Environment.NewLine);
+            storedProcedureCode.Append("END" + Environment.NewLine);
+            migrationBuilder.Sql(storedProcedureCode.ToString());
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -1004,6 +1022,9 @@ namespace NCCRD.Services.DataV2.Migrations
 
             migrationBuilder.DropTable(
                 name: "ProjectType");
+
+            //DROP POLYGON SP
+            migrationBuilder.Sql("DROP PROCEDURE dbo.PolygonFilter");
         }
     }
 }
---
+++ NCCRD_API/NCCRD.Services.DataV2/Migrations/20181024123826_Add Polygon SP.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Text;
+
+namespace NCCRD.Services.DataV2.Migrations
+{
+    public partial class AddPolygonSP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            StringBuilder storedProcedureCode = new StringBuilder();
+
+            storedProcedureCode.Append("CREATE PROCEDURE dbo.PolygonFilter" + Environment.NewLine);
+            storedProcedureCode.Append("@WKT VARCHAR(MAX)" + Environment.NewLine);
+            storedProcedureCode.Append("AS" + Environment.NewLine);
+            storedProcedureCode.Append("BEGIN" + Environment.NewLine);
+            storedProcedureCode.Append("DECLARE @QUERY VARCHAR(MAX) = " + Environment.NewLine);
+            storedProcedureCode.Append("'SELECT Project.*" + Environment.NewLine);
+            storedProcedureCode.Append("FROM  Location" + Environment.NewLine);
+            storedProcedureCode.Append("INNER JOIN ProjectLocation ON Location.LocationId = ProjectLocation.LocationId" + Environment.NewLine);
+            storedProcedureCode.Append("INNER JOIN Project ON ProjectLocation.ProjectId = Project.ProjectId" + Environment.NewLine);
+            storedProcedureCode.Append("WHERE geometry::STGeomFromText(''~WKT~'', 0).MakeValid().STIntersects(geometry::STGeomFromText(''POINT('' + CAST(Location.LonCalculated as VARCHAR(MAX)) + '' '' + CAST(Location.LatCalculated as VARCHAR(MAX)) + '')'', 0)) = 1';" + Environment.NewLine);
+            storedProcedureCode.Append("SET @QUERY = REPLACE(@QUERY, '~WKT~', @WKT)" + Environment.NewLine);
+            storedProcedureCode.Append("EXEC(@QUERY);" + Environment.NewLine);
+            storedProcedureCode.Append("END" + Environment.NewLine);
+
+            migrationBuilder.Sql(storedProcedureCode.ToString());
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE dbo.PolygonFilter");
+        }
+    }
+}
